746e69cdf554bbd966023cb15f7dd01b86adb092
Vibyird <vibyird@gmail.com>
2025-08-08 16:19:48 +0800

    [WN]新增highlightjs

--- docs/javascripts/extra.js
+++
@@ -1,8 +0,0 @@
-hljs.highlightAll()
-// Register event handlers after documented loaded
-window.addEventListener('pushState', function () {
-  hljs.highlightAll()
-})
-window.addEventListener('replaceState', function () {
-  hljs.highlightAll()
-})
