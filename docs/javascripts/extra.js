712920ffdd3124d8dcea339fee1cdebaa15fa490
Vibyird <vibyird@gmail.com>
2025-08-08 15:17:03 +0800

    [WN]新增highlightjs

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,5 +1,5 @@
-hljs.initHighlightingOnLoad()
+hljs.highlightAll()
 // Register event handlers after documented loaded
 document.addEventListener('DOMContentLoaded', function () {
-  hljs.initHighlightingOnLoad()
+  hljs.highlightAll()
 })
