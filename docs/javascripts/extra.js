f7c744348d489ac136d58dc6a24629ca81bd7099
Vibyird <vibyird@gmail.com>
2025-08-12 09:12:08 +0800

    [RC]优化写法

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -3,8 +3,8 @@ document$.subscribe(function () {
   if (palette && typeof palette.color === 'object') {
     var theme = palette.color.scheme === 'slate' ? 'dark' : 'light'
 
-    document.querySelector('link[href*=github\\.min\\.css]').disabled = theme == 'dark'
-    document.querySelector('link[href*=github-dark\\.min\\.css]').disabled = theme == 'light'
+    document.querySelector('link[href*=github\\.min\\.css]').disabled = theme != 'light'
+    document.querySelector('link[href*=github-dark\\.min\\.css]').disabled = theme != 'dark'
   }
   hljs.highlightAll()
 })
@@ -16,8 +16,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var palette = __md_get('__palette')
     if (palette && typeof palette.color === 'object') {
       var theme = palette.color.scheme === 'slate' ? 'dark' : 'light'
-      document.querySelector('link[href*=github\\.min\\.css]').disabled = theme == 'dark'
-      document.querySelector('link[href*=github-dark\\.min\\.css]').disabled = theme == 'light'
+
+      document.querySelector('link[href*=github\\.min\\.css]').disabled = theme != 'light'
+      document.querySelector('link[href*=github-dark\\.min\\.css]').disabled = theme != 'dark'
     }
   })
 })
